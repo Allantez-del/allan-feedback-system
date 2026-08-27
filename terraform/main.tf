6c85a2996d479432dea908e17e6029d3223cc438
Allan Mwangi <allankago16@gmail.com>
2026-08-27 21:12:27 +0200

    Add Terraform DynamoDB infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+﻿resource "aws_dynamodb_table" "feedback" {
+  name         = "allan-feedback"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "feedbackId"
+
+  attribute {
+    name = "feedbackId"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  tags = {
+    Project     = "Allan Feedback System"
+    Environment = "dev"
+  }
+}
