4d2bd51e3b4616e1a54c270f2178c0d8b98e581e
Allan Mwangi <allankago16@gmail.com>
2026-08-27 22:08:14 +0200

    Add API Gateway feedback endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,3 +90,49 @@ resource "aws_lambda_function" "feedback" {
     Environment = "dev"
   }
 }
+
+resource "aws_apigatewayv2_api" "feedback_api" {
+  name          = "allan-feedback-api"
+  protocol_type = "HTTP"
+
+  tags = {
+    Project     = "Allan Feedback System"
+    Environment = "dev"
+  }
+}
+
+resource "aws_apigatewayv2_integration" "feedback_lambda" {
+  api_id = aws_apigatewayv2_api.feedback_api.id
+
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.feedback.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "post_feedback" {
+  api_id = aws_apigatewayv2_api.feedback_api.id
+
+  route_key = "POST /feedback"
+  target    = "integrations/${aws_apigatewayv2_integration.feedback_lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id = aws_apigatewayv2_api.feedback_api.id
+
+  name        = "$default"
+  auto_deploy = true
+
+  tags = {
+    Project     = "Allan Feedback System"
+    Environment = "dev"
+  }
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.feedback.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.feedback_api.execution_arn}/*/*"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,4 @@
-
+output "api_endpoint" {
+  description = "Base URL of the Customer Feedback API"
+  value       = aws_apigatewayv2_api.feedback_api.api_endpoint
+}
